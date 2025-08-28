1920135a9268c00415fd3d0f7449670416582ae6
CassienSin <cassiensin@gmai.com>
2025-08-28 11:43:02 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeHflQEzI6dW15tu1VyA2ZuhDoMgVLUGs",
+  authDomain: "aeb-goals-tracker.firebaseapp.com",
+  projectId: "aeb-goals-tracker",
+  storageBucket: "aeb-goals-tracker.firebasestorage.app",
+  messagingSenderId: "330077982447",
+  appId: "1:330077982447:web:c7d19db908a53298e7951a"
+};
+
+
+const app = initializeApp(firebaseConfig);
